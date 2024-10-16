7b5d1d6070c00469df8e12cecfa6ade277af170d
YoanBosko <yoanbosko@gmail.com>
2024-10-16 23:53:37 +0700

    Initial commit

---
+++ Assets/Script/ChangeScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveTo(string parameter1)
+    {
+        SceneManager.LoadScene(parameter1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        // UnityEditor.EditorApplication.ExitPlaymode();
+    }
+}
